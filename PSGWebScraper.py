e9477b13e9a8920a5dccbe776c440672f6f4a8c0
Joanna <joannakwh@gmail.com>
2020-07-03 15:26:41 -0700

    update README and add comments

--- PSGWebScraper.py
+++ PSGWebScraper.py
@@ -7,6 +7,9 @@ import time
 from pandas import Series, DataFrame
 from lxml import html
 
+"""Makes url requet and return a byte tree
+
+"""
 def makeRequest(year, make, model):
     url = 'http://www.plussizingguide.com/cgi-bin/ultimate_guide_online.cgi'
     params = {'host': 'HTTP_HOST', 'userName': 'cariboo',
@@ -18,29 +21,39 @@ def makeRequest(year, make, model):
     if (year is not None and len(year) != 0):
         params.update({'selectModel': model})
     result = requests.post(url, data=params)
-    # print(result.text)
-    tree = html.fromstring(result.content)
+    tree = html.fromstring(result.content)      # use page.content rather than page.text because html.fromstring implicitly expects bytes as input
     return tree
 
+"""Makes request and returns years
+
+"""
 def getYears():
     tree = makeRequest(None, None, None)
-    years = tree.xpath('//select[@name="selectYear"]/option/text()')
-    # remove 'Select Year' option
-    years.pop(0)
+    years = tree.xpath('//select[@name="selectYear"]/option/text()')     # traverse raw html output to select specific tags to get year string
+    years.pop(0)    # remove 'Select Year' option
     return years
 
+"""Makes request and returns makes
+Parameters: year    
+"""
 def getMakes(year):
     tree = makeRequest(str(year), None, None)
     makes = tree.xpath('//select[@name="selectMake"]/option/text()')
     makes.pop(0)
     return makes
 
+"""Makes request and returns models
+Parameters: year, make    
+"""
 def getModels(year, make):
     tree = makeRequest(str(year), str(make), None)
     models = tree.xpath('//select[@name="selectModel"]/option/text()')
     models.pop(0)
     return models
 
+"""Makes request and returns car info as dictionary pairs
+Parameters: year, make model    
+"""
 def getInfo(year, make, model):
     info = {}
     tree = makeRequest(str(year), str(make), str(model))
@@ -61,9 +74,13 @@ for year in years:
         print(make)
         models = getModels(int(year), make)
         for model in models: 
+            # get one row of information with year, make, model
             info = getInfo(int(year), make, model)
+            # make DataFrame with dictionary pairs with values as the data, and keys as index
             data = pd.DataFrame(data=info.values(), index=info.keys()).transpose()
+            # merge new row with rest of data
             df = pd.concat([df, data], axis=0)
             print(data)
+
 df.to_csv('output.csv', index = False)
 
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # PSGWebScraper
+
+This script uses scrapes all tire information for each year, make, model of car for a specific tire guide service and exports  the data into a .csv file
+The user is assumed to be subscribed to the tire guide service and must provide their own api keys
+
+## Main python libraries used
+* Pandas
+* XPath
